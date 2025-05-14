4b4e2fcb272e95f89ee1ac90c4a17fbc93bc0f07
Andrew Goldie <goldie@worksend.com>
2025-05-15 09:41:46 +1200

    update dependency after debugging session for dwn

--- src/did/document.rs
+++ src/did/document.rs
@@ -11,7 +11,7 @@ use anyhow::{anyhow, bail};
 use base64ct::{Base64UrlUnpadded, Encoding};
 use chrono::{DateTime, Utc};
 use credibil_jose::PublicKeyJwk;
-use credibil_se::{derive_x25519, X25519_CODEC};
+use credibil_se::{derive_x25519_public, PublicKey, X25519_CODEC};
 use multibase::Base;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -753,12 +753,13 @@ impl VerificationMethod {
             }
         };
         let key_bytes = Base64UrlUnpadded::decode_vec(&jwk.x)?;
-        let x25519_bytes = derive_x25519(&key_bytes)?;
+        let pub_key = PublicKey::from_slice(&key_bytes)?;
+        let x25519_key = derive_x25519_public(&pub_key)?;
 
         // base58B encode the raw key
         let mut multi_bytes = vec![];
         multi_bytes.extend_from_slice(&X25519_CODEC);
-        multi_bytes.extend_from_slice(&x25519_bytes);
+        multi_bytes.extend_from_slice(&x25519_key.to_bytes());
         let multikey = multibase::encode(Base::Base58Btc, &multi_bytes);
 
         let vm = VerificationMethodBuilder::new(&PublicKeyFormat::PublicKeyMultibase {
