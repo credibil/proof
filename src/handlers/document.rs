6af067849d2b4e909946a189a7e572af6ae0230e
andrewweston <andrew.weston@propellerhead.co.nz>
2025-06-23 18:48:05 +1200

    make provider trait clearer

--- src/handlers/document.rs
+++ src/handlers/document.rs
@@ -26,25 +26,24 @@ pub struct DocumentResponse(pub Document);
 /// Returns an `OpenID4VP` error if the request is invalid or if the provider is
 /// not available.
 async fn document(
-    _owner: &str, provider: &impl Provider, request: DocumentRequest,
+    _owner: &str, proof: &impl Proof, request: DocumentRequest,
 ) -> Result<DocumentResponse> {
     let url = request.url.trim_end_matches("/did.json").trim_end_matches("/.well-known");
     let did = create_did(url)?;
 
-    let document = Proof::get(provider, &did, &did)
-        .await?
-        .ok_or_else(|| anyhow!("document not found for did: {did}"))?;
+    let document =
+        proof.get(&did, &did).await?.ok_or_else(|| anyhow!("document not found for did: {did}"))?;
 
     Ok(DocumentResponse(document))
 }
 
-impl<P: Provider> Handler<DocumentResponse, P> for Request<DocumentRequest> {
+impl<P: Proof> Handler<DocumentResponse, P> for Request<DocumentRequest> {
     type Error = Error;
 
     async fn handle(
-        self, owner: &str, provider: &P,
+        self, owner: &str, proof: &P,
     ) -> Result<impl Into<Response<DocumentResponse>>, Self::Error> {
-        document(owner, provider, self.body).await
+        document(owner, proof, self.body).await
     }
 }
 
